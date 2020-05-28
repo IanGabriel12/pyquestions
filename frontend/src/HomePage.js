943b12d4f8037191c2ca2cdf46883827fc3fb01f
Ian Gabriel <ianeigor12@gmail.com>
2020-05-28 00:32:50 -0300

    landing page header

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HomePage.css';
 import pythonLogo from './assets/images/python.png';
+import brandBg from './assets/images/brand-bg.png';
 import pcBoy from './assets/images/pc-boy.png';
 import pcGirl from './assets/images/pc-girl.png';
 
@@ -10,6 +11,7 @@ export default function HomePage(props) {
       <div className='decoration-rows'></div>
       <header>
          <div className='brand'>
+            <img src={brandBg} className='bg'/>
             <div className='logo'>
                <img src={pythonLogo} alt='Python logo'/>
                <span>PyQuestions</span>
@@ -19,21 +21,7 @@ export default function HomePage(props) {
             <a href='#'>Banco de questões</a>
             <a href='#'>Sobre nós</a>
          </nav>
-      </header>
-
-      <div className='banner'>
-         <h1>Bem-vindo ao seu site de estudos!</h1>
-         <div className='img-wrapper'>
-            <img src={pcBoy} alt='Boy programing' className='banner-image' id='pc-boy'/>
-            <img src={pcGirl} alt='Girl programing' className='banner-image' id='pc-girl'/>
-         </div>
-         
-         <div className='background-gradient'></div>
-      </div>
-      <footer>
-         <a href='#'>Tente novas questões</a>
-         <a href='#'>Prepare-se para olimpíadas</a>
-      </footer>   
+      </header> 
       </>
    )
 }
