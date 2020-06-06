d29b65e69e69dff536bb35e01ccbf6db43bd330b
Ian Gabriel <ianeigor12@gmail.com>
2020-06-06 16:13:37 -0300

    moving files

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var cors = require('cors');
-var requireDir = require('require-dir');
-
-var app = express();
-app.use(cors());
-app.use(express.json());
-
-app.listen(3307);
