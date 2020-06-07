631c3d7c8d4dda3c54ed67573b9e5e8f01f31ea1
Ian Gabriel <ianeigor12@gmail.com>
2020-06-06 22:51:43 -0300

    removed database from repos

--- backend/src/database/seeds/003_input_examples.js
+++ backend/src/database/seeds/003_input_examples.js
@@ -3,17 +3,13 @@ const questions = require("../data/questions.json").questions;
 exports.seed = async function(knex) {
    let initialQuestionID = 1;
 
-   // each question has an array of input examples, we reduce to a single array
-   // representing all input examples 
-   const examplesToSeed = questions.reduce((examplesArray, question, questionIndex) => {
-      const serializedInputExamples = question.input_examples.map(example => ({
-         input: example.input,
-         output: example.output,
-         question_id: initialQuestionID + questionIndex
-      }));
-
-      return examplesArray.concat(serializedInputExamples);
-   }, []);
+   const examplesToSeed = questions.flatMap((question, index) => (
+      question.input_examples.map(input_example => ({
+         input: input_example.input,
+         output: input_example.output,
+         question_id: initialQuestionID + index,
+      }))
+   )) 
 
    return knex('input_examples').del()
       .then(() => {
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,14 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(
-   '/resolutions', 
+   '/resolutions',
    express.static(path.resolve(__dirname, '..', 'assets', 'resolutions'))
 )
 
 app.get('/chapters', ChaptersController.index) //list all chapters
 app.get('/chapters/:id', ChaptersController.show) //get a chapter and return the questions from it
 app.get('/questions/:id', QuestionsController.show) //get detail from a question
-
- //get a resolution file
 
 app.listen(3307);
